19bd7a0f27c85ded0e5f5d5c25e86740e80b2918
zksystem <zksystem@yahoo.com>
2023-01-15 14:48:33 +0300

    Cell layout design

---
+++ ImageFeed/ImagesListCell.swift
@@ -0,0 +1,12 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Konstantin Zuykov on 15.01.2023.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+}
