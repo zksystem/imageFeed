526efce10eac12e6b2f296393fa9c241d0f586a1
zksystem <zksystem@yahoo.com>
2023-01-15 15:23:15 +0300

    ImagesFeed format, likes, data

--- ImagesList/ImagesListCell.swift
+++ ImagesList/ImagesListCell.swift
@@ -9,4 +9,7 @@ import UIKit
 
 final class ImagesListCell: UITableViewCell {
     static let reuseIdentifier = "ImagesListCell"
+    @IBOutlet var cellImage: UIImageView!
+    @IBOutlet var likeButton: UIButton!
+    @IBOutlet var dateLabel: UILabel!
 }
