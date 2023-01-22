cad7364c2686bca9def80dfdecdaf90045ba4f26
zksystem <zksystem@yahoo.com>
2023-01-22 11:41:41 +0300

    added profile view controller

---
+++ Profile/ProfileViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Konstantin Zuykov on 22.01.2023.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+}
