5276ccc98e1f3e850520d302078ee0ee96a21251
zksystem <zksystem@yahoo.com>
2023-01-22 19:16:12 +0300

    Profile view outlets and actions was added

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -9,7 +9,13 @@ import UIKit
 
 final class ProfileViewController: UIViewController {
     
+    @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var logoutButton: UIButton!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var userIdLabel: UILabel!
+    @IBOutlet weak var greetMsgLabel: UILabel!
     
-    
+    @IBAction func logoutButtonAction(_ sender: Any) {
+    }
     
 }
