e7cf739465bc42b0196cce317617f789ddc1fa40
zksystem <zksystem@yahoo.com>
2023-01-29 09:31:00 +0300

    SingleView was added

---
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Konstantin Zuykov on 29.01.2023.
+//
+
+import UIKit
+
+
+final class SingleImageViewController : UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+}
