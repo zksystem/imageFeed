e384ffad5280deb516090c2038905b2840bab4fa
zksystem <zksystem@yahoo.com>
2023-02-05 17:33:52 +0300

    Added constants

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Konstantin Zuykov on 05.02.2023.
+//
+
+import Foundation
+
+let AccessKey = "xLx62HweIHcH5LmEAd30FNsNOmzYAJqRPvx8oeSiRIo"
+let SecretKey = "qxANFgGaoB6PDL6SENsSvrXxrjeLdd-nLuYtiZkHbiM"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
