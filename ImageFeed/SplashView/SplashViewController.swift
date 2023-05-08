8733dd245921078c072a54c22197af23e3d81adc
zksystem <zksystem@yahoo.com>
2023-05-08 12:15:21 +0300

    code cleanup

--- ImageFeed/SplashView/SplashViewController.swift
+++ ImageFeed/SplashView/SplashViewController.swift
@@ -9,28 +9,50 @@ import UIKit
 import ProgressHUD
 
 final class SplashViewController: UIViewController {
-
-    //static let shared = OAuth2Service()
-
+    
     private let oAuth2TokenStorage = OAuth2TokenStorage()
-
     private let oAuth2Service = OAuth2Service.shared
     private let profileService = ProfileService.shared
     private let profileImageService = ProfileImageService.shared
-
+    
     private let showAuthenticationScreenSegueIdentifier = "ShowAuthenticationScreen"
     
+    private var splashLogoImage: UIImageView = {
+        let image = UIImage(named: "splash_logo")
+        let imageView = UIImageView(image: image)
+        return imageView
+    }()
+    
+    
+    // MARK: Init splash scree image
+    
+    private func initSplashImage() {
+        splashLogoImage.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(splashLogoImage)
+        NSLayoutConstraint.activate([
+            splashLogoImage.heightAnchor.constraint(equalToConstant: 78),
+            splashLogoImage.widthAnchor.constraint(equalToConstant: 75),
+            splashLogoImage.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            splashLogoImage.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    // MARK: view did load
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        view.backgroundColor = UIColor(named: "ypBlack")
+        initSplashImage()
     }
     
+    // MARK: view did appear
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         checkAuthToken()
     }
+    
+    // MARK: Check auth token
     
     private func checkAuthToken() {
         if oAuth2TokenStorage.token != nil {
@@ -39,45 +61,26 @@ final class SplashViewController: UIViewController {
             }
             UIBlockingProgressHUD.show()
             fetchProfile(token: token)
-
-
         } else {
-            //performSegue(withIdentifier: showAuthenticationScreenSegueIdentifier, sender: nil)
             let storyboard = UIStoryboard(name: "Main", bundle: .main)
             guard let authViewController =
                     storyboard.instantiateViewController(withIdentifier: "AuthViewController") as? AuthViewController else {
                 return
             }
-
+            
             authViewController.delegate = self
             authViewController.modalPresentationStyle = .fullScreen
             self.present(authViewController, animated: true)
         }
     }
     
+    
+    
     private func switchToTabBarController() {
-        
         guard let window = UIApplication.shared.windows.first else { fatalError("Invalid Configuration") }
         let tabBarController = UIStoryboard(name: "Main", bundle: .main)
             .instantiateViewController(withIdentifier: "TabBarViewController")
         window.rootViewController = tabBarController
-    }
-}
-
-extension SplashViewController {
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == showAuthenticationScreenSegueIdentifier {
-            guard
-                let navigationController = segue.destination as? UINavigationController,
-                let viewController = navigationController.viewControllers[0] as? AuthViewController
-            else {
-                assertionFailure("Failed to prepare for \(showAuthenticationScreenSegueIdentifier)")
-                return
-            }
-            viewController.delegate = self
-        } else {
-            super.prepare(for: segue, sender: sender)
-        }
     }
 }
 
@@ -108,7 +111,8 @@ extension SplashViewController: AuthViewControllerDelegate {
         }
     }
     
-   
+    // MARK: Fetching profile
+    
     private func fetchProfile(token: String) {
         profileService.fetchProfile(token) { [weak self] result in
             guard let self = self else {
@@ -125,7 +129,6 @@ extension SplashViewController: AuthViewControllerDelegate {
                 break
             }
         }
-    }
-    
+    }    
     
 }
---
+++ ImageFeed/TabBarController/TabBarController.swift
@@ -0,0 +1,25 @@
+//
+//  TabBarController.swift
+//  ImageFeed
+//
+//  Created by Konstantin Zuykov on 08.05.2023.
+//
+
+import UIKit
+
+final class TabBarController: UITabBarController {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        let storyboard = UIStoryboard(name: "Main", bundle: .main)
+        let imagesListViewController = storyboard.instantiateViewController(withIdentifier: "ImagesListViewController")
+        let profileViewController = ProfileViewController()
+        
+        profileViewController.tabBarItem = UITabBarItem(
+            title: nil,
+            image: UIImage(named: "tabProfile"),
+            selectedImage: nil
+        )
+        self.viewControllers = [imagesListViewController, profileViewController]
+    }
+}
+
